a4a21029a38bb9f807916d283bb6a22b147f231b
Akhmad Khalid Yusuf <161675087+jalgitap@users.noreply.github.com>
2024-05-22 18:10:03 +0700

    Add files via upload

---
+++ cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir /etc/certificates
+touch /etc/domain
+domain=$(cat /etc/domain)
+wget https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
